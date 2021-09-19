8435c43bee80be65b381caa264a4a8c6134e22e8
Abdul Fakhri <abf.ca@hotmail.com>
2021-09-19 12:56:59 -0700

    up

--- campaign/jscode.php
+++ campaign/jscode.php
@@ -21,20 +21,14 @@ if(empty($_SESSION["username"]))
                             $style=$camp['selected_style'];
                             $startDate=$camp['date_reg'];
                             $user=$camp['user_key'];
+                            $base_url=$_SERVER['HTTP_REFERER'];
 
                             $js_code= '<iframe width="300" height="90" 
 style="position:absolute;bottom:5;left:5;border:1px solid black;border-radius: 20px;" 
-src="http://demo.regrowup.com/popup/fetch_data.php?q=2" title="YouTube video player" frameborder="1" allow="accelerometer; 
+src='.$base_url.'/popup/fetch_data.php?q='.$user.'" title="Javascript Code" frameborder="1" allow="accelerometer; 
 autoplay; clipboard-write;encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
 </iframe>';
 
-                            
-
-
-
-                             
-                              // echo $js_code; 
-                                    
 $myVar = htmlentities($js_code, ENT_QUOTES);
 echo($myVar);
 
