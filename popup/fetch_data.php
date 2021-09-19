f3f8c6a9f3a577acf9fb73c650818be1ac379884
Abdul Fakhri <abf.ca@hotmail.com>
2021-09-18 23:01:53 -0700

    updated

--- popup/fetch_data.php
+++ popup/fetch_data.php
@@ -1,13 +1,10 @@
 <?php
-        /*
+        
         $connect = new PDO("mysql:host=localhost; dbname=u587940520_sticky", "u587940520_sticky_review", "!@#123qweasdZXC");
 
         $qw=$_GET['q'];
-		$query = "
-		SELECT *
-		FROM sticky_review WHERE st_user='$qw'
-		ORDER BY id DESC
-		LIMIT 1";
+
+		$query = "SELECT * FROM sticky_review WHERE st_user='$qw' ORDER BY st_id DESC LIMIT 1";
 
 		$result = $connect->query($query);
 
@@ -16,8 +13,8 @@
 			echo $row['st_name'];
 			
 		}
-	    */
-        echo "Hi";
+	    
+        
 
 	
 
