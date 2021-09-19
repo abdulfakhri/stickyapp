1791219cd283ad24bfc868495257a32ab53c4d18
Abdul Fakhri <abf.ca@hotmail.com>
2021-09-18 23:15:32 -0700

    updated

--- popup/fetch_data.php
+++ popup/fetch_data.php
@@ -1,12 +1,12 @@
-<?php
-        
+<?php  
+/*
         $connect = new PDO("mysql:host=localhost; dbname=u587940520_sticky", "u587940520_sticky_review", "!@#123qweasdZXC");
 
         $qw=$_GET['q'];
 
 		//$query = "SELECT * FROM sticky_review WHERE st_user='$qw' ORDER BY st_id DESC LIMIT 1";
 
-        $query = "SELECT * FROM sticky_review ";
+        $query = "SELECT * FROM sticky_review";
 
 		$result = $connect->query($query);
 
@@ -15,10 +15,40 @@
 			echo $row['st_name'];
 			
 		}
-	    
-        
-
-	
-
-
+        */
 ?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$servername = "localhost";
+$username = "u587940520_sticky_review";
+$password = "!@#123qweasdZXC";
+$dbname = "u587940520_sticky";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM sticky_review";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["st_id"]. " - Name: " . $row["st_name"]. " " . $row["st_desc"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+?>
+
+</body>
+</html>
