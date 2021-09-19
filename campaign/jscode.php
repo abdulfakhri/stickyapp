4ea0d025c1155e3f4f62f8be520887cb9e387b14
Abdul Fakhri <abf.ca@hotmail.com>
2021-09-19 12:59:35 -0700

    up

--- campaign/jscode.php
+++ campaign/jscode.php
@@ -18,14 +18,35 @@ if(empty($_SESSION["username"]))
 
                             foreach($camps as $camp) : 
 
+                                
+// Program to display URL of current page.
+
+if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+	$link = "https";
+else
+	$link = "http";
+
+// Here append the common URL characters.
+$link .= "://";
+
+// Append the host(domain name, ip) to the URL.
+$link .= $_SERVER['HTTP_HOST'];
+
+// Append the requested resource location to the URL
+$link .= $_SERVER['REQUEST_URI'];
+	
+// Print the link
+//echo $link;
+
+
                             $style=$camp['selected_style'];
                             $startDate=$camp['date_reg'];
                             $user=$camp['user_key'];
-                            $base_url=$_SERVER['HTTP_REFERER'];
+                            $base_url=$link;
 
                             $js_code= '<iframe width="300" height="90" 
 style="position:absolute;bottom:5;left:5;border:1px solid black;border-radius: 20px;" 
-src='.$base_url.'/popup/fetch_data.php?q='.$user.'" title="Javascript Code" frameborder="1" allow="accelerometer; 
+src='.$link.'/popup/fetch_data.php?q='.$user.'" title="Javascript Code" frameborder="1" allow="accelerometer; 
 autoplay; clipboard-write;encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
 </iframe>';
 
