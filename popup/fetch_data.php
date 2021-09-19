d90fa5db652991270d8fdc2c80d3bff985871c94
Abdul Fakhri <abf.ca@hotmail.com>
2021-09-18 22:36:42 -0700

    updated with popup

--- popup/fetch_data.php
+++ popup/fetch_data.php
@@ -9,7 +9,7 @@ $connect = new PDO("mysql:host=localhost; dbname=rgudb", "root", "root");
         $qw=$_GET['q'];
 		$query = "
 		SELECT *
-		FROM sticky_reviews WHERE user_key='$qw'
+		FROM sticky_review WHERE st_user='$qw'
 		ORDER BY id DESC
 		LIMIT 1";
 
@@ -17,7 +17,7 @@ $connect = new PDO("mysql:host=localhost; dbname=rgudb", "root", "root");
 
 		foreach($result as $row)
 		{
-			echo $row['stars'];
+			echo $row['st_name']." ".$row['st_desc'];
 			
 		}
 	
