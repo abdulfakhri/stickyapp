362f504ef52901de7d39f7c16a3cb1c944105a5a
Abdul Fakhri <abf.ca@hotmail.com>
2021-09-18 23:40:57 -0700

    up

--- popup/fetch_data.php
+++ popup/fetch_data.php
@@ -44,7 +44,7 @@ if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
         echo "id: ".$row["st_id"]." - Name: ".$row["st_name"]." ".$row["st_desc"]."<br>";
-        $up="uploads/reviews/".$row["st_image"];
+        $up="/reviews/uploads/".$row["st_image"];
         echo "<img src=".$up.">";
     }
 } else {
